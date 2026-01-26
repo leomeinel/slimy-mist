bafea02b9ed594a66effcc27ecf50f412082e2ab
Leopold Johannes Meinel <leo@meinel.dev>
2026-01-26 20:54:59 +0100

    version: Update to 0.12.1

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -46,9 +46,9 @@ android {
         minSdk = 31
         targetSdk = 36
         // NOTE: Increase by 1 on each release
-        versionCode = 11
+        versionCode = 12
         // NOTE: Update with full semantic version on each release
-        versionName = "0.12.0"
+        versionName = "0.12.1"
         // https://developer.android.com/reference/tools/gradle-api/8.13/com/android/build/api/variant/ExternalNativeBuild
         // NOTE: We need this, otherwise libc++_shared.so will not be inserted
         @Suppress("UnstableApiUsage")

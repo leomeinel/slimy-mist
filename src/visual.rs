0c933af65afbe0c3857a14a7c6c6e20f6a00a195
Leopold Johannes Meinel <leo@meinel.dev>
2026-02-12 19:17:30 +0100

    Comments: Mention milestone in FIXME for visual

--- src/visual.rs
+++ src/visual.rs
@@ -10,10 +10,9 @@
 // FIXME: I'd like to have an easy way to use shaders for my sprites. This does currently not seem possible.
 //        We also need this for showing an outline.
 //        Also see:
-//        - https://github.com/bevyengine/bevy/issues/1738
-//        - https://github.com/bevyengine/bevy/issues/7131
 //        - https://github.com/merwaaan/bevy_spritesheet_animation/issues/66
 //        - https://github.com/bevyengine/bevy/pull/22484 (merged)
+//            - Part of: https://github.com/bevyengine/bevy/milestone/40 (0.19)
 
 pub(crate) mod layers;
 pub(crate) mod particles;

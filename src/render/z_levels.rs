a0eb8ca138bddfa8fa10bf1c127ceb216f6c9fef
Leopold Johannes Meinel <leo@meinel.dev>
2026-05-10 15:19:04 +0200

    comments: Add note to LIGHT_Z

--- src/render/z_levels.rs
+++ src/render/z_levels.rs
@@ -7,6 +7,8 @@ pub(crate) const LEVEL_Z: f32 = 1.;
 pub(crate) const FOREGROUND_Z: f32 = 5.;
 /// Z-level for any overlay object.
 pub(crate) const OVERLAY_Z: f32 = 9.;
+// NOTE: Since light is rendered in a full screen render pass, this currently has no effect.
+//       Multiplicative blending is missing from bevy 2d, therefore this can't be avoided in bevy_fast_light.
 /// Z-level for light.
 pub(crate) const LIGHT_Z: f32 = 10.;
 
